47e9864f091ebc6e184a716f522126d9845568ba
luidimso <luidi_mso@outlook.com>
2021-03-15 11:27:38 -0300

    Starting login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_animations/pages/login/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Animations',
       debugShowCheckedModeBanner: false,
-      home: LogoApp()
+      // home: LogoApp()
+      home: LoginPage()
     );
   }
 }
---
+++ lib/pages/login/login_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_animations/components/form_component.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/background.jpeg"),
+            fit: BoxFit.cover
+          )
+        ),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: 70,
+                        bottom: 32
+                      ),
+                      child: Image.asset("images/logo.png",
+                        width: 150,
+                        height: 150,
+                        fit: BoxFit.contain
+                      ),
+                    ),
+                    FormComponent()
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
