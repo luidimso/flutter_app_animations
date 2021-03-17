5106f1e742e8412914088761522a26908c446013
luidimso <luidi_mso@outlook.com>
2021-03-17 11:14:14 -0300

    Animation to shrink button

--- lib/components/btnAnimation_component.dart
+++ lib/components/btnAnimation_component.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 
 class BtnAnimationComponent extends StatelessWidget {
   final AnimationController animationController;
+  final Animation<double> buttonAnimation;
 
-  BtnAnimationComponent({this.animationController});
+  BtnAnimationComponent({this.animationController}) : buttonAnimation = Tween(
+    begin: 320.0,
+    end: 60.0
+  ).animate(CurvedAnimation(
+    parent: animationController,
+    curve: Interval(0.0, 0.15)
+  ));
 
   @override
   Widget build(BuildContext context) {
@@ -17,25 +24,38 @@ class BtnAnimationComponent extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.only(bottom: 50),
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          animationController.forward();
+        },
         child: Container(
-          width: 320,
-          height: 69,
+          width: buttonAnimation.value,
+          height: 60,
           alignment: Alignment.center,
           decoration: BoxDecoration(
             color: Color.fromRGBO(247, 67, 106, 1.0),
             borderRadius: BorderRadius.all(Radius.circular(30))
           ),
-          child: Text("Sign in",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.w300,
-              letterSpacing: 0.3
-            ),
-          ),
+          child: _buildInsideBtn(context),
         ),
       ),
     );
+  }
+
+  Widget _buildInsideBtn(BuildContext context) {
+    if(buttonAnimation.value > 75) {
+      return Text("Sign in",
+        style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w300,
+            letterSpacing: 0.3
+        ),
+      );
+    } else {
+      return CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+        strokeWidth: 1,
+      );
+    }
   }
 }
